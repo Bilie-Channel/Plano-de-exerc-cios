92d8b44920e9a81c157d5985d9c684299668b842
Bilie-Channel <108486284+Bilie-Channel@users.noreply.github.com>
2026-01-06 22:05:27 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -9,7 +9,15 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('notificationclick', event => {
   event.notification.close();
-  event.waitUntil(
-    clients.openWindow('/')
-  );
+  event.waitUntil(clients.openWindow('/'));
+});
+
+// Recebe mensagens para notificações de teste
+self.addEventListener('message', event => {
+  if(event.data && event.data.type === 'TEST_NOTIFICATION'){
+    self.registration.showNotification('Teste de Notificação', {
+      body: event.data.body || 'Esta é uma notificação de teste do PWA!',
+      icon: 'icon-192.png'
+    });
+  }
 });
